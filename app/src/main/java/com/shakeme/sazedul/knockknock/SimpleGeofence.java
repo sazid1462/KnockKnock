3e34570f4861ca5bee698079a0815a219d84a980
sazid1462 <sazidmailbox@gmail.com>
2014-12-08 10:14:30 +0600

    Before implementing SimpleGeofence and SimpleGeofenceStore implemented

---
+++ app/src/main/java/com/shakeme/sazedul/knockknock/SimpleGeofence.java
@@ -0,0 +1,75 @@
+package com.shakeme.sazedul.knockknock;
+
+import com.google.android.gms.location.Geofence;
+
+/**
+ * Created by Sazedul on 08-Dec-14.
+ */
+public class SimpleGeofence {
+    // Instance variables
+    private final String mId;
+    private final Double mLatitude;
+    private final Double mLongitude;
+    private final float mRadius;
+    private long mExpirationDuration;
+    private int mTransitionType;
+
+    /**
+     *
+     * @param mId The Geofence's request ID
+     * @param mLatitude Latitude of the Geofence's center
+     * @param mLongitude Longitude of the Geofence's center
+     * @param mRadius Radius of the Geofence circle
+     * @param mExpirationDuration Geofence expiration duration
+     * @param mTransitionType Type of Geofence transition
+     */
+    public SimpleGeofence(String mId, Double mLatitude, Double mLongitude, float mRadius, long mExpirationDuration, int mTransitionType) {
+        // Set the instance fields from the constructor
+        this.mId = mId;
+        this.mLatitude = mLatitude;
+        this.mLongitude = mLongitude;
+        this.mRadius = mRadius;
+        this.mExpirationDuration = mExpirationDuration;
+        this.mTransitionType = mTransitionType;
+    }
+
+    // Instance field getters
+    public String getId() {
+        return mId;
+    }
+
+    public Double getLatitude() {
+        return mLatitude;
+    }
+
+    public Double getLongitude() {
+        return mLongitude;
+    }
+
+    public float getRadius() {
+        return mRadius;
+    }
+
+    public long getExpirationDuration() {
+        return mExpirationDuration;
+    }
+
+    public int getTransitionType() {
+        return mTransitionType;
+    }
+
+    /**
+     * Creates a Location Services Geofence object from a SimpleGeofence.
+     *
+     * @return A Geofence object
+     */
+    public Geofence toGeofence() {
+        // Build a new Geofence object
+        return new Geofence.Builder()
+                .setRequestId(getId())
+                .setCircularRegion(getLatitude(), getLongitude(), getRadius())
+                .setExpirationDuration(getExpirationDuration())
+                .setTransitionTypes(getTransitionType())
+                .build();
+    }
+}
---
+++ app/src/main/java/com/shakeme/sazedul/knockknock/SimpleGeofenceStore.java
@@ -0,0 +1,115 @@
+package com.shakeme.sazedul.knockknock;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+/**
+ * Created by Sazedul on 08-Dec-14.
+ *
+ * Storage for geofence values, implemented in SharedPreferences
+ */
+public class SimpleGeofenceStore {
+    // Keys for flattened geofences stored in SharedPreferences
+    public static final String KEY_LATITUDE = "com.shakeme.sazedul.knockknock.KEY_LATITUDE";
+    public static final String KEY_LONGITUDE = "com.shakeme.sazedul.knockknock.KEY_LONGITUDE";
+    public static final String KEY_RADIUS = "com.shakeme.sazedul.knockknock.KEY_RADIUS";
+    public static final String KEY_EXPIRATION_DURATION = "com.shakeme.sazedul.knockknock.KEY_EXPIRATION_DURATION";
+    public static final String KEY_TRANSITION_TYPE = "com.shakeme.sazedul.knockknock.KEY_TRANSITION_TYPE";
+    public static final String KEY_PREFIX = "com.shakeme.sazedul.knockknock.KEY";
+
+    /*
+     * Invalid values used to test geofence storage when retrieving geofences
+     */
+    public static final long INVALID_LONG_VALUE = -999l;
+    public static final float INVALID_FLOAT_VALUE = -999.0f;
+    public static final int INVALID_INT_VALUE = -999;
+
+    // The SharedPreferences object in which geofences are stored
+    private final SharedPreferences mPrefs;
+    // The name of the SharedPreferences
+    private static final String SHARED_PREFERENCES = "KnockKnockSharedPreferences";
+    // Create the SharedPreferences with private access only
+    public SimpleGeofenceStore(Context context) {
+        mPrefs = context.getSharedPreferences(SHARED_PREFERENCES, Context.MODE_PRIVATE);
+    }
+
+    /**
+     * Returns a stored geofence by its id, or returns null if it's not found.
+     *
+     * @param id The id of a stored geofence
+     * @return A geofence defined by its center and radius.
+     */
+    public SimpleGeofence getGeofence(String id) {
+        /*
+         * Get the latitude for the geofence identified by id, or
+         * INVALID_FLOAT_VALUE if it doesn't exist
+         */
+        double lat = mPrefs.getFloat(getGeofenceFieldKey(id, KEY_LATITUDE), INVALID_FLOAT_VALUE);
+        /*
+         * Get the longitude for the geofence identified by id, or
+         * INVALID_FLOAT_VALUE if it doesn't exist
+         */
+        double lng = mPrefs.getFloat(getGeofenceFieldKey(id, KEY_LONGITUDE), INVALID_FLOAT_VALUE);
+        /*
+         * Get the radius for the geofence identified by id, or
+         * INVALID_FLOAT_VALUE if it doesn't exist
+         */
+        float radius = mPrefs.getFloat(getGeofenceFieldKey(id, KEY_RADIUS), INVALID_FLOAT_VALUE);
+        /*
+         * Get the expiration duration for the geofence identified by id, or
+         * INVALID_LONG_VALUE if it doesn't exist
+         */
+        long expirationDuration = mPrefs.getLong(getGeofenceFieldKey(id, KEY_EXPIRATION_DURATION), INVALID_LONG_VALUE);
+        /*
+         * Get the transition type for the geofence identified by id, or
+         * INVALID_INT_VALUE if it doesn't exist
+         */
+        int transitionType = mPrefs.getInt(getGeofenceFieldKey(id, KEY_TRANSITION_TYPE), INVALID_INT_VALUE);
+
+        // If none of the values is incorrect, return the object
+        if (lat != INVALID_FLOAT_VALUE && lng != INVALID_FLOAT_VALUE &&
+                expirationDuration != INVALID_LONG_VALUE &&
+                radius != INVALID_FLOAT_VALUE && transitionType != INVALID_INT_VALUE) {
+            // Return a true Geofence object
+            return new SimpleGeofence(id, lat, lng, radius, expirationDuration, transitionType);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Save a geofence.
+     *
+     * @param id ID of the geofence
+     * @param geofence The SimpleGeofence containing the values
+     *                 you want to save in SharedPreferences
+     */
+    public void setGeofence(String id, SimpleGeofence geofence){
+        /*
+         * Get a SharedPreferences editor instance. Among other things,
+         * SharedPreferences ensures that updates are atomic and non-concurrent
+         */
+        Editor editor = mPrefs.edit();
+        // Write the Geofence vaues to SharedPreferences
+        editor.putFloat(getGeofenceFieldKey(id, KEY_LATITUDE), geofence.getLatitude().floatValue());
+        editor.putFloat(getGeofenceFieldKey(id, KEY_LONGITUDE), geofence.getLongitude().floatValue());
+        editor.putFloat(getGeofenceFieldKey(id, KEY_RADIUS), geofence.getRadius());
+        editor.putLong(getGeofenceFieldKey(id, KEY_EXPIRATION_DURATION), geofence.getExpirationDuration());
+        editor.putInt(getGeofenceFieldKey(id, KEY_TRANSITION_TYPE), geofence.getTransitionType());
+        // Commit the changes
+        editor.apply();
+    }
+
+    /**
+     * Given a Geofence object's ID and the name of the field (for example, KEY_LATITUDE),
+     * return the key name of the object's values in SharedPreferences
+     *
+     * @param id The id of a geofence object
+     * @param fieldName The field represented by the key
+     * @return The full key name of a value in SharedPreferences
+     */
+    private String getGeofenceFieldKey(String id, String fieldName) {
+        return KEY_PREFIX + "_" + id + "_" + fieldName;
+    }
+}
